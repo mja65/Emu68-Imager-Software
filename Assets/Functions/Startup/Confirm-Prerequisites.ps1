392ae46b32f69915288d27a3d5be7617872c0283
mja65 <supremeturnip@hotmail.com>
2025-10-14 08:36:26 +1100

    Update Confirm-Prerequisites.ps1
    Fixed typos

--- Assets/Functions/Startup/Confirm-Prerequisites.ps1
+++ Assets/Functions/Startup/Confirm-Prerequisites.ps1
@@ -3,25 +3,25 @@ function Confirm-Prerequisites {
     )
     
     $is64bit = Test-64bit
-    $isAministrator = Test-Administrator
+    $isAdministrator = Test-Administrator
     $FailedCheck = 0
         
     if ($is64bit -eq $false){
         $FailedCheck ++
     }
-    if ($isAministrator  -eq $false){
+    if ($isAdministrator  -eq $false){
         $FailedCheck ++
     }
     
     if ($FailedCheck -gt 0){
         $WPF_FailedPrerequisite = Get-XAML -WPFPrefix 'WPF_PreRequisiteCheck_' -XMLFile '.\Assets\WPF\Window_FailedPrerequisite.xaml' -ActionsPath '.\Assets\UIActions\PreRequisiteCheck\' -AddWPFVariables
-        if (($is64bit -eq $false) -and ($isAministrator  -eq $false)){
+        if (($is64bit -eq $false) -and ($isAdministrator  -eq $false)){
             $WPF_PreRequisiteCheck_TextBox_Message.Text = 'You must run the tool in Administrator Mode and using a 64bit OS!'
         }
-        elseif (($is64bit -eq $false) -and ($isAministrator  -eq $true)) {
+        elseif (($is64bit -eq $false) -and ($isAdministrator  -eq $true)) {
             $WPF_PreRequisiteCheck_TextBox_Message.Text = 'You must run the tool using a 64bit OS!'
         }
-        elseif (($isAministrator  -eq $false) -and ($is64bit -eq $true))  {
+        elseif (($isAdministrator  -eq $false) -and ($is64bit -eq $true))  {
             $WPF_PreRequisiteCheck_TextBox_Message.Text = 'You must run the tool in Administrator Mode!'
         }
         $null = $WPF_FailedPrerequisite.ShowDialog()
