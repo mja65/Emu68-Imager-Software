cb8b41be14268f0f76097745ac2288243e7d70a0
mja65 <supremeturnip@hotmail.com>
2025-10-21 18:43:45 +1100

    Formatting of version check

--- Script/Main.ps1
+++ Script/Main.ps1
@@ -106,7 +106,7 @@ elseif ($Script:GUICurrentStatus.OperationMode -eq "Simple"){
 }
 
 $WPF_Window_Label_Title.Content = "Emu68 Imager v$([string]$Script:Settings.Version)" 
-$WPF_Window_Label_VersionStatus.Content = Get-Emu68ImagerCurrentVersion -GithubRelease "https://api.github.com/repos/mja65/Emu68Imager/releases" 
+$WPF_Window_Label_VersionStatus.Text = Get-Emu68ImagerCurrentVersion -GithubRelease "https://api.github.com/repos/mja65/Emu68Imager/releases" 
 
 If ($Script:GUICurrentStatus.OperationMode -eq "Advanced"){
     $WPF_StartPage = Get-XAML -WPFPrefix 'WPF_StartPage_' -XMLFile '.\Assets\WPF\Grid_StartPageAdvancedMode.xaml' -ActionsPath '.\Assets\UIActions\StartPage\' -AddWPFVariables
