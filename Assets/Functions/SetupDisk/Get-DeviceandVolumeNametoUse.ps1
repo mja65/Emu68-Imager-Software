f0fc58fc8012fee4ddf7ecfaf310693213696033
mja65 <supremeturnip@hotmail.com>
2025-10-22 21:31:03 +1100

    Update Get-DeviceandVolumeNametoUse.ps1
    Fix to automatic iteration for Amiga devicename and volumename

--- Assets/Functions/SetupDisk/Get-DeviceandVolumeNametoUse.ps1
+++ Assets/Functions/SetupDisk/Get-DeviceandVolumeNametoUse.ps1
@@ -8,32 +8,50 @@ function Get-DeviceandVolumeNametoUse {
         VolumeName = $null
     }
     
-    $VolumeNamePrefix = "$((Get-InputCSVs -Diskdefaults | Where-Object {$_.Disk -eq "Work"}).VolumeName)_"
-    $DeviceNamePrefix = (Get-InputCSVs -Diskdefaults | Where-Object {$_.Disk -eq "Work"}).devicename -replace "\d", "" 
-    $DeviceNumbertoUse = 0
-    $VolumeNameCountertoUse = 0
-    
-    $Script:GUICurrentStatus.AmigaPartitionsandBoundaries | ForEach-Object {
-        $DevicetoCheck = $_.Partition.devicename 
-        $VolumeNametoCheck = $_.Partition.VolumeName 
-        if ($VolumeNametoCheck -match "Work_"){
-            if (([int]($VolumeNametoCheck -replace "Work_","")) -gt $VolumeNameCountertoUse){
-                $VolumeNameCountertoUse = [int]($VolumeNametoCheck -replace "Work_","")
-            }
-        }
-        if (($DevicetoCheck -replace "\d", "") -eq $DeviceNamePrefix){
-            if ([int]($DevicetoCheck -replace "\D", "") -gt $DeviceNumbertoUse){
-                $DeviceNumbertoUse = [int]($DevicetoCheck -replace "\D")
-            }
-    
-        } 
-    }
-    
-    $DeviceNumbertoUse ++
-    $VolumeNameCountertoUse ++
-    
-    $OutputtoReturn.DeviceName = "$DeviceNamePrefix$DeviceNumbertoUse"
-    $OutputtoReturn.VolumeName = "$VolumeNamePrefix$VolumeNameCountertoUse"
+    $VolumeNameSystem = "$((Get-InputCSVs -Diskdefaults | Where-Object {$_.Disk -eq "System"}).VolumeName)"
+    $VolumeNameWork = "$((Get-InputCSVs -Diskdefaults | Where-Object {$_.Disk -eq "Work"}).VolumeName)"
 
+    $VolumeNameWorkPrefix = "$($VolumeNameWork)_"
+    $DeviceNamePrefix = (Get-InputCSVs -Diskdefaults | Where-Object {$_.Disk -eq "Work"}).devicename -replace "\d", "" 
+
+    
+    $DeviceNumbertoUse = 0
+    $IsDeviceNumberFound = $false
+
+    ($Script:GUICurrentStatus.AmigaPartitionsandBoundaries | Select-Object 'Partition').Partition.DeviceName | Sort-Object | ForEach-Object {
+        $DeviceNameNumber = $_ -replace $DeviceNamePrefix, ""
+        if (($DeviceNumbertoUse -ne $DeviceNameNumber) -and ($IsDeviceNumberFound -eq $false)) {
+            $IsDeviceNumberFound = $true
+            $OutputtoReturn.DeviceName = "$DeviceNamePrefix$DeviceNumbertoUse"             
+        }
+        $DeviceNumbertoUse ++
+    }
+
+    if ($IsDeviceNumberFound -eq $false){
+        $OutputtoReturn.DeviceName = "$DeviceNamePrefix$DeviceNumbertoUse" 
+    }
+
+    if (-not (($Script:GUICurrentStatus.AmigaPartitionsandBoundaries | Select-Object 'Partition').Partition.VolumeName | Where-Object {$_ -eq $VolumeNameWork})) {
+        $OutputtoReturn.VolumeName =  $VolumeNameWork
+    } 
+
+    if (-not ($OutputtoReturn.VolumeName)){
+        $VolumeNameCountertoUse = 1
+        $IsVolumeNameFound = $false
+        ($Script:GUICurrentStatus.AmigaPartitionsandBoundaries | Select-Object 'Partition').Partition.VolumeName | Where-Object {($_ -ne $VolumeNameSystem) -and ($_ -ne $VolumeNameWork) -and ($_ -match $VolumeNameWorkPrefix)}  | Sort-Object | ForEach-Object {
+           $VolumeCountertoCheck = [int]($_-replace $VolumeNameWorkPrefix,"")
+           if (($VolumeNameCountertoUse -ne $VolumeCountertoCheck) -and ($IsVolumeNameFound -eq $false)){
+               $IsVolumeNameFound = $true
+               $OutputtoReturn.VolumeName = "$VolumeNameWorkPrefix$VolumeNameCountertoUse"
+           }
+           $VolumeNameCountertoUse ++
+        
+        }
+        
+        if ($IsVolumeNameFound -eq $false){
+            $OutputtoReturn.VolumeName = "$VolumeNameWorkPrefix$VolumeNameCountertoUse"
+        }
+
+    }
     return $OutputtoReturn
 }
