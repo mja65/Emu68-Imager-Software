312fdc311f6bdce252afa0d34b31b6f6edd0dacc
mja65 <supremeturnip@hotmail.com>
2025-10-31 09:50:02 +1100

    Remove import partition code

--- Assets/UIActions/DiskPartitionImportDisk/WPF_DP_ID_Cancel_Button.ps1
+++
@@ -1,5 +0,0 @@
-$WPF_DP_ID_Cancel_Button.Add_Click({
-   # $Script:GUIActions.ActionToPerform = $null
-   $Script:GUICurrentStatus.ProcessImportedPartition = $false
-    $WPF_SelectDiskWindow.Close()
-})
