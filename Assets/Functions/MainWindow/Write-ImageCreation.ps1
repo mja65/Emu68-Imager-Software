a7912021fe54b8cea4fbcbf7f864d9f3495a1243
mja65 <supremeturnip@hotmail.com>
2025-11-10 07:46:50 +1100

    Added entry to log to show disk size for Image and for available disks

--- Assets/Functions/MainWindow/Write-ImageCreation.ps1
+++ Assets/Functions/MainWindow/Write-ImageCreation.ps1
@@ -149,6 +149,8 @@ function Write-ImageCreation {
         $Script:Settings.CurrentSubTaskNumber ++
         $Script:Settings.CurrentTaskName = 'Processing Commands on Disk'
 
+        Write-InformationMessage -Message "Disk size (Bytes) is: $($WPF_DP_Disk_GPTMBR.DiskSizeBytes)"
+
         Write-StartSubTaskMessage
         $HSTCommandstoRun = $Script:GUICurrentStatus.HSTCommandstoProcess.DiskStructures + $Script:GUICurrentStatus.HSTCommandstoProcess.WriteFilestoDisk
         Start-HSTCommands -HSTScript $HSTCommandstoRun -Section "DiskStructures;WriteFilestoDisk" -ActivityDescription 'Processing commands' -ReportTime
@@ -157,6 +159,9 @@ function Write-ImageCreation {
      Write-TaskCompleteMessage 
      
      if ($OutputTypetoUse -eq 'VHDImage'){
+
+        Write-InformationMessage -Message "Disk size (Bytes) is: $($WPF_DP_Disk_GPTMBR.DiskSizeBytes)"
+        
         $IsMounted = (Get-DiskImage -ImagePath $Script:GUIActions.OutputPath -ErrorAction Ignore).Attached
         if ($IsMounted -eq $true){
            Write-InformationMessage -Message "Dismounting existing image: $($Script:GUIActions.OutputPath)"
