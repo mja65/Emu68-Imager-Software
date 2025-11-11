96e8615af6d70790d2a4503b9d855f80586de434
mja65 <supremeturnip@hotmail.com>
2025-11-11 19:52:53 +1100

    Update Write-HSTcommandstoLog.ps1

--- Assets/Functions/Startup/Write-HSTcommandstoLog.ps1
+++ Assets/Functions/Startup/Write-HSTcommandstoLog.ps1
@@ -5,16 +5,32 @@ function Write-HSTCommandstoLog {
     param (
         
         )
-        $FullListofCommands =   $Script:GUICurrentStatus.HSTCommandstoProcess.ExtractOSFiles +`
-                            $Script:GUICurrentStatus.HSTCommandstoProcess.CopyIconFiles + `
-                            $Script:GUICurrentStatus.HSTCommandstoProcess.NewDiskorImage +`
-                            $Script:GUICurrentStatus.HSTCommandstoProcess.DiskStructures + `
-#                                $Script:GUICurrentStatus.HSTCommandstoProcess.CopyImportedFiles +`
-                            $Script:GUICurrentStatus.HSTCommandstoProcess.WriteFilestoDisk +`
-                            $Script:GUICurrentStatus.HSTCommandstoProcess.AdjustParametersonImportedRDBPartitions       
-                            
-                            
+                                     
     "HST imager commands ran:" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
-     $FullListofCommands.Command | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "ExtractOS Files" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    $Script:GUICurrentStatus.HSTCommandstoProcess.ExtractOSFiles | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "Copy Icon Files" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    $Script:GUICurrentStatus.HSTCommandstoProcess.CopyIconFiles | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "New Disk or Image" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    $Script:GUICurrentStatus.HSTCommandstoProcess.NewDiskorImage | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "Disk Structures" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    $Script:GUICurrentStatus.HSTCommandstoProcess.DiskStructures | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "Write Files to Disk" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8     
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8      
+    $Script:GUICurrentStatus.HSTCommandstoProcess.WriteFilestoDisk | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    "Adjust Parameters on Imported RDB Partitions" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8     
+    "" |Out-File $Script:Settings.LogLocation -Append -Encoding utf8
+    $Script:GUICurrentStatus.HSTCommandstoProcess.AdjustParametersonImportedRDBPartitions | Out-File $Script:Settings.LogLocation -Append -Encoding utf8 -Width 2000      
+    
 
     }              
