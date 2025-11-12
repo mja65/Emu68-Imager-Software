4caf78756525917529fcc1dddd60f7ce4dd92763
mja65 <supremeturnip@hotmail.com>
2025-11-13 07:52:52 +1100

    Update Copy-CDFiles.ps1

--- Assets/Functions/ProcessInstallFiles/Write-AmigaFilestoInterimDrive/Copy-CDFiles.ps1
+++ Assets/Functions/ProcessInstallFiles/Write-AmigaFilestoInterimDrive/Copy-CDFiles.ps1
@@ -35,27 +35,27 @@ function Copy-CDFiles {
         Copy-Item -Path "$TempFoldertoExtract$FiletoExtract" -Destination "$OutputDirectory\$NewFileName" -Force -Recurse 
     }
     else {
-        if ((Split-Path -path $FiletoExtract -Leaf) -match "Aux"){
-            if ((Split-Path -path $FiletoExtract -Leaf) -eq "Aux"){
-                $FiletoExtracttoUse  = "$(Split-Path $FiletoExtract -Parent)\_$(Split-Path $FiletoExtract -Leaf)"
-            }
-            else {
-                $FiletoExtracttoUse = $FiletoExtract
-            }
-            $Script:GUICurrentStatus.HSTCommandstoProcess.CDExtractionCommands += [PSCustomObject]@{
-                Command = "fs mkdir `"$OutputDirectory`""
-                Sequence = 0
-            }
-            $Script:GUICurrentStatus.HSTCommandstoProcess.CDExtractionCommands += [PSCustomObject]@{
-                Command = "fs copy `"$TempFoldertoExtract$FiletoExtracttoUse`" `"$OutputDirectory`" --uaemetadata None --recursive FALSE --makedir FALSE"
-                Sequence = 1
-            }
+        # if ((Split-Path -path $FiletoExtract -Leaf) -match "Aux"){
+        #     if ((Split-Path -path $FiletoExtract -Leaf) -eq "Aux"){
+        #         $FiletoExtracttoUse  = "$(Split-Path $FiletoExtract -Parent)\_$(Split-Path $FiletoExtract -Leaf)"
+        #     }
+        #     else {
+        #         $FiletoExtracttoUse = $FiletoExtract
+        #     }
+        #     $Script:GUICurrentStatus.HSTCommandstoProcess.CDExtractionCommands += [PSCustomObject]@{
+        #         Command = "fs mkdir `"$OutputDirectory`""
+        #         Sequence = 0
+        #     }
+        #     $Script:GUICurrentStatus.HSTCommandstoProcess.CDExtractionCommands += [PSCustomObject]@{
+        #         Command = "fs copy `"$TempFoldertoExtract$FiletoExtracttoUse`" `"$OutputDirectory`" --uaemetadata None --recursive FALSE --makedir FALSE"
+        #         Sequence = 1
+        #     }
     
-        }
-        else {
+        # }
+        # else {
             Write-InformationMessage -Message "Copying file $TempFoldertoExtract$FiletoExtract to $OutputDirectory"
             Copy-Item -Path "$TempFoldertoExtract$FiletoExtract" -Destination $OutputDirectory -Force -Recurse 
-        }
+        # }
     }
     return $true
 
