002b0a97eed2e6e4423442f3b87339fd625d2564
mja65 <matt.alexandre@gmail.com>
2025-12-08 17:25:21 +1100

    Revised warning for Framethrower

--- Assets/UIActions/StartPage/WPF_StartPage_Unicam_CheckBox.ps1
+++ Assets/UIActions/StartPage/WPF_StartPage_Unicam_CheckBox.ps1
@@ -1,9 +1,24 @@
 $WPF_StartPage_Unicam_CheckBox.add_Checked({
+    $MessageHeader = "Framethrower Enabled"
     if ($WPF_StartPage_ScreenMode_Dropdown.SelectedItem -eq 'Automatic'){
-        $MessageHeader = "Non Optimum ScreenMode"
-        $MessageBody = "You have automatic screenmode selected. If you are using Framethrower you should select a specific screenmode (ideally 50hz for PAL content)"
-        $null = Show-WarningorError -Msg_Header $MessageHeader -Msg_Body $MessageBody -BoxTypeWarning -ButtonType_OK
+        $MessageBody = @"
+You have enabled Unicam settings for Framethrower. Framethrower is still in development and you may need to update Emu68 to the latest version manually in order for it to work properly. Also, if you enable Unicam where you are not actually using it you will notice graphical anomalies when the Amiga switches screenmodes. 
+
+You have also selected automatic screenmode. If you are using Framethrower ideally you should select a specific screenmode (50hz for PAL content).
+
+Please visit the PiStorm discord and the #framethrower-support channel if you need help.
+"@        
+
     }
+    else{
+        $MessageBody = @"
+You have enabled Unicam settings for Framethrower. Framethrower is still in development and you may need to update Emu68 to the latest version manually in order for it to work properly. Also, if you enable Unicam where you are not actually using it you will notice graphical anomalies when the Amiga switches screenmodes. 
+
+Please visit the PiStorm discord and the #framethrower-support channel if you need help.
+"@    
+    
+    }
+$null = Show-WarningorError -Msg_Header $MessageHeader -Msg_Body $MessageBody -BoxTypeWarning -ButtonType_OK
     $WPF_StartPage_Unicam_CheckBox.IsEnabled = 1
     $Script:GUIActions.UnicamEnabled = $true
     $Script:GUIActions.UnicamStartonBoot = $true
