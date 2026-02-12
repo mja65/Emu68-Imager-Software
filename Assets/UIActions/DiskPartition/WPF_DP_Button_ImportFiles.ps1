d1c719341a2274c521ecb3555489b7ff9a7f447d
mja65 <matt.alexandre@gmail.com>
2026-02-12 16:18:31 +1100

    Update WPF_DP_Button_ImportFiles.ps1
    Prevent user from importing files from reserved folder

--- Assets/UIActions/DiskPartition/WPF_DP_Button_ImportFiles.ps1
+++ Assets/UIActions/DiskPartition/WPF_DP_Button_ImportFiles.ps1
@@ -15,14 +15,33 @@ $WPF_DP_Button_ImportFiles.add_click({
                 Show-WarningorError -Msg_Header 'Imported Partition Selected' -Msg_Body 'Cannot transfer files to the this partition!' -BoxTypeError -ButtonType_OK
             }
             else {
-                $PathtoPopulate = Get-FolderPath -Message 'Select path to files to import' -InitialDirectory ([System.IO.Path]::GetFullPath($Script:Settings.DefaultInstallMediaLocation)) 
+                $PathtoPopulate = Get-FolderPath -Message 'Select path to files to import' -InitialDirectory ([System.IO.Path]::GetFullPath($Script:Settings.DefaultImportLocation)) 
                 if ($PathtoPopulate){
+                    $Separator = [System.IO.Path]::DirectorySeparatorChar
+                    $ReservedPaths = @(
+                        $Script:Settings.MiamiFilesLocation
+                        $Script:Settings.RoadshowFilesLocation
+                        $Script:Settings.DefaultOutputImageLocation
+                        $Script:Settings.DefaultInstallMediaLocation
+                        $Script:Settings.DefaultROMLocation
+                        $Script:Settings.DownloadedFileSystems
+                    ) | ForEach-Object { [System.IO.Path]::GetFullPath($_).TrimEnd($Separator) + $Separator }
+                        
+                    $PathtoCheck = [System.IO.Path]::GetFullPath($PathtoPopulate).TrimEnd($Separator) + $Separator
+
+                    if ($ReservedPaths | Where-Object {$PathtoCheck.StartsWith($_) }) {
+                        $CannotUseFolder = $true
+                        $Script:GUICurrentStatus.FileBoxOpen = $false
+                        Show-WarningorError -Msg_Header 'Reserved Folder Location' -Msg_Body 'Cannot import files from this folder Location!' -BoxTypeError -ButtonType_OK
+                    } 
+                    
                     $SpaceofFolderBytes = (Get-ChildItem -Path $PathtoPopulate -recurse | Measure-Object -Property Length -Sum).Sum
                     if ($SpaceofFolderBytes -gt $PartitionSizeByes){
+                        $CannotUseFolder = $true
                         $Script:GUICurrentStatus.FileBoxOpen = $false
                         Show-WarningorError -Msg_Header 'Insufficient Space' -Msg_Body 'Insufficient space on partition for files!' -BoxTypeError -ButtonType_OK
                     }
-                    else {
+                    If ($CannotUseFolder -ne $true) {
                         $Script:GUICurrentStatus.SelectedAmigaPartition.ImportedFilesPath = $PathtoPopulate 
                         $Script:GUICurrentStatus.SelectedAmigaPartition.ImportedFilesSpaceBytes = $SpaceofFolderBytes
                         $Script:GUICurrentStatus.FileBoxOpen = $false
